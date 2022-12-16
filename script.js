7706683405b5d4152148859f0e19a0f9ddc2eda5
f-adi <fadi.baaklini@gmail.com>
2022-12-16 10:41:33 -0500

    Passing Arguments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -26,3 +27,37 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('Check In');
+  } else {
+    alert('Wrong Passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000000);
+  // console.log(person.passport);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
