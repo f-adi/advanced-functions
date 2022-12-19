9c6f77035d7f6c5987b6217466fa6f1f78f0820d
f-adi <fadi.baaklini@gmail.com>
2022-12-19 12:44:25 -0500

    call and apply method

--- script.js
+++ script.js
@@ -61,7 +61,6 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 
-*/
 
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
@@ -77,7 +76,7 @@ const upperFirstWord = function (str) {
 const transformer = function (str, fn) {
   console.log(`Original string ${str}`);
   console.log(`Transformed string: ${fn(str)}`);
-
+  
   console.log(`Transformed by: ${fn.name}`);
 };
 
@@ -93,3 +92,70 @@ const high5 = function () {
 document.body.addEventListener('click', high5);
 
 ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet(`Hey`);
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')(`Jonas`);
+
+// Challenge
+
+let greetArrow = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArrow('Hi')(`Jonas`);
+*/
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// book(23, 'Sarah Williams');
+
+//Call Method
+
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+
+//Apply Method
+const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData);
+
+book.call(swiss, ...flightData);
+console.log(swiss);
